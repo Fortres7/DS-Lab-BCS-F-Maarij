cbb09ee545abc54b0bad6b311fa16397bb66585e
Maarij <127932628+Fortres7@users.noreply.github.com>
2025-11-18 22:37:48 +0500

    Create 4.cpp

---
+++ Lab 10/4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i)
+{
+  int largest = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+
+  if (left < n && arr[left] > arr[largest])
+    largest = left;
+
+  if (right < n && arr[right] > arr[largest])
+    largest = right;
+
+  if (largest != i)
+  {
+    swap(arr[i], arr[largest]);
+    heapify(arr, n, largest);
+  }
+}
+
+void ConvertMinToMax(int arr[], int n)
+{
+  for (int i = n / 2 - 1; i >= 0; i--)
+  {
+    heapify(arr, n, i);
+  }
+}
+
+int main()
+{
+  int arr[] = {1, 3, 5, 7, 9, 8};
+  int n = 6;
+
+  cout << "Min Heap: ";
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  ConvertMinToMax(arr, n);
+
+  cout << "Max Heap: ";
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
